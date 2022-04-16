544a065cccaa4ccf1019c03e4ef8a591e41e079b
BECKY-OCHOLLA <becky.ocholla@student.moringaschool.com>
2022-04-16 22:46:13 +0300

    it will add add functions to repo search component

--- src/app/repository-search/repository-search.component.ts
+++ src/app/repository-search/repository-search.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { RepositoryUserService } from '../repository-user.service';
 
 @Component({
   selector: 'app-repository-search',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RepositorySearchComponent implements OnInit {
 
-  constructor() { }
+  //access form inputs
+  @ViewChild('f') searchRepositoryForm!: NgForm;
 
-  ngOnInit(): void {
+  repositories: any;
+  searchText!: string;
+  displayUserRepositoryList = false;
+  displayUserErrorMessage = false;
+  repositoryUserService: any;
+  constructor(private userservice: RepositoryUserService) { }
+
+  ngOnInit(): void {}
+
+  //search for github repositories
+  searchGithubUserRepositories() {
+    this.searchText = this.searchRepositoryForm.value.search;
+    this.userservice.getRepositoryResponse(this.searchText).then(
+      (response) => {
+        this.repositories = this.userservice.getRepositoryDetails;
+        this.displayUserRepositoryList = true;
+      },
+      (error) => {
+        this.displayUserErrorMessage = true;
+        console.log(error);
+      }
+    );
   }
-
 }
